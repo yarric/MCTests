6f307e02291b35d76076f23f07d821ee8bf5a87a
yari <ya@gm.com>
2017-03-07 19:54:20 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: yari
+ *
+ * Created on March 7, 2017, 7:49 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf ("Hello, world!");
+
+    return (EXIT_SUCCESS);
+}
+
